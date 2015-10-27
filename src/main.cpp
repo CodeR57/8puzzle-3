964a2e2e5c5a2c94434ccae5559721000d62c85c
Lauren <lboyd014@gmail.com>
2015-10-26 21:25:15 -0700

    expand function working

--- src/main.cpp
+++ src/main.cpp
@@ -37,30 +37,22 @@ class Compare_Values    {
 
 //OPERATORS: i and j are row and column values you want to move blank to
 Matrix UP(Matrix& M, int i, int j){
-    int temp = M.problem[i][j];
-    M.problem[i][j] = 0;//M.problem[i+1][j];
-    M.problem[i+1][j] = temp;
+    swap(M.problem[i][j], M.problem[i+1][j]);
     return M;
 }
 
 Matrix DN(Matrix& M, int i, int j){
-    int temp = M.problem[i][j];
-    M.problem[i][j] = 0;//M.problem[i-1][j];
-    M.problem[i-1][j] = temp;
+    swap(M.problem[i][j],M.problem[i-1][j]);
     return M;
 }
 
 Matrix RT(Matrix& M, int i, int j){
-    int temp = M.problem[i][j];
-    M.problem[i][j] = 0;//M.problem[i][j-1];
-    M.problem[i][j-1] = temp;
+    swap(M.problem[i][j], M.problem[i][j-1]);
     return M;
 }
 
 Matrix LF(Matrix& M, int i, int j){
-    int temp = M.problem[i][j];
-    M.problem[i][j] = 0;//M.problem[i][j+1];
-    M.problem[i][j+1] = temp;
+    swap(M.problem[i][j], M.problem[i][j+1]);
     return M;
 }
 
@@ -68,55 +60,24 @@ Matrix LF(Matrix& M, int i, int j){
 //returns queue of matricies (all possible ways to move the blank)
 vector< Matrix >  expand(Matrix& M, Matrix& M1, Matrix& M2, Matrix& M3, Matrix& M4){
     vector< Matrix > vec;
-        
-    //Matrix M1; 
-    //M1.problem.push_back(std::vector<int>(3, 1));
-    //M1.problem.push_back(std::vector<int>(3, 2));
-    //M1.problem.push_back(std::vector<int>(3, 3));
+    M1.problem = M.problem;
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            M1.problem[i][j]= M.problem[i][j];
-            cout << M1.problem[i][j] << "\t"; 
-        }
-        cout << endl;
-    }
-    cout << endl;
-    
-    //Matrix M2;
-     
-    //M2.problem.push_back(std::vector<int>(3, 1));
-    //M2.problem.push_back(std::vector<int>(3, 2));
-    //M2.problem.push_back(std::vector<int>(3, 3));
+    // cout << "copy matrix: " << endl;
+    //for(int i = 0; i < 3; i++){
+    //    for(int j = 0; j < 3; j++){
+    //        //M1.problem[i][j]= M.problem[i][j];
+    //        cout << M1.problem[i][j] << "\t"; 
+    //    }
+    //  cout << endl;
+    //}
+    //cout << endl;
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            M2.problem[i][j]= M.problem[i][j];
-        }
-    }
-    
-    //Matrix M3; 
-    //M3.problem.push_back(std::vector<int>(3, 1));
-    //M3.problem.push_back(std::vector<int>(3, 2));
-    //M3.problem.push_back(std::vector<int>(3, 3));
+    M2.problem = M.problem;
 
     //Matrix M3;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            M3.problem[i][j]= M.problem[i][j];
-        }
-    }
-   
-    //Matrix M4; 
-    //M4.problem.push_back(std::vector<int>(3, 1));
-    //M4.problem.push_back(std::vector<int>(3, 2));
-    //M4.problem.push_back(std::vector<int>(3, 3));
+    M3.problem = M.problem;
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            M4.problem[i][j]= M.problem[i][j];
-        }
-    }
+    M4.problem = M.problem; 
 
     int row,column;
     for(int i = 0; i < 3; i++){
@@ -128,7 +89,7 @@ vector< Matrix >  expand(Matrix& M, Matrix& M1, Matrix& M2, Matrix& M3, Matrix& 
             }
         }
     }
-    
+
     cout << "Blank is at row: " << row << " and column: " << column << endl;
 
     if(column == 2 && row == 0){
@@ -136,40 +97,40 @@ vector< Matrix >  expand(Matrix& M, Matrix& M1, Matrix& M2, Matrix& M3, Matrix& 
         vec.push_back(DN(M2,row+1,column));
     }
     else if(column == 2 && row == 1){
-        vec.push_back(UP(M,row-1,column));
+        vec.push_back(UP(M1,row-1,column));
         vec.push_back(LF(M2, row, column-1));
         vec.push_back(DN(M3, row+1, column));
     }
     else if(column ==2 && row ==2){
-        vec.push_back(UP(M, row-1, column));
+        vec.push_back(UP(M1, row-1, column));
         vec.push_back(LF(M2, row, column -1));
     }
     else if(column == 0 && row == 0){
-        vec.push_back(RT(M,row, column+1));
+        vec.push_back(RT(M1,row, column+1));
         vec.push_back(DN(M2,row+1,column));
     }
     else if(column == 0 && row == 1){
-        vec.push_back(UP(M,row-1,column));
+        vec.push_back(UP(M1,row-1,column));
         vec.push_back(RT(M2, row, column+1));
         vec.push_back(DN(M3, row+1, column));
     }
     else if(column ==0 && row ==2){
-        vec.push_back(UP(M, row-1, column));
+        vec.push_back(UP(M1, row-1, column));
         vec.push_back(RT(M2, row, column+1));
     }
     else if(column == 1 && row == 0){
-        vec.push_back(LF(M,row, column-1));
+        vec.push_back(LF(M1,row, column-1));
         vec.push_back(DN(M2,row+1,column));
         vec.push_back(RT(M3, row, column+1));
     }
     else if(column == 1 && row == 1){
-        vec.push_back(UP(M,row-1,column));
+        vec.push_back(UP(M1,row-1,column));
         vec.push_back(LF(M2, row, column-1));
         vec.push_back(DN(M3, row+1, column));
         vec.push_back(RT(M4, row, column+1));
     }
     else if(column ==1 && row ==2){
-        vec.push_back(UP(M, row-1, column));
+        vec.push_back(UP(M1, row-1, column));
         vec.push_back(LF(M2, row, column -1));
         vec.push_back(RT(M3, row, column + 1));
     }
@@ -205,12 +166,12 @@ int main(){
     M1.problem.push_back(std::vector<int>(3, 1));
     M1.problem.push_back(std::vector<int>(3, 2));
     M1.problem.push_back(std::vector<int>(3, 3));
-    
+
     Matrix M2;
     M2.problem.push_back(std::vector<int>(3, 1));
     M2.problem.push_back(std::vector<int>(3, 2));
     M2.problem.push_back(std::vector<int>(3, 3));
-    
+
     Matrix M3;
     M3.problem.push_back(std::vector<int>(3, 1));
     M3.problem.push_back(std::vector<int>(3, 2));
@@ -249,12 +210,12 @@ int main(){
     M.problem[0][1]=3;
     M.problem[0][2]=2;
     M.problem[1][0]=4;
-    M.problem[1][1]=5;
+    M.problem[1][1]=0;
     M.problem[1][2]=6;
     M.problem[2][0]=7;
-    M.problem[2][1]=0;
+    M.problem[2][1]=5;
     M.problem[2][2]=8;
-    
+
     cout << "Outputting regular matrix: " << endl;
     int c,d;
     for(d = 0; d < 3; ++d)
@@ -267,14 +228,14 @@ int main(){
     vector<Matrix> vec = expand(M, M1, M2, M3, M4);
 
 
-    cout << "Outputting the que: " << endl;
+    cout << "Outputting the queue: " << endl;
     int i,e,f;
     for(i = 0; i < vec.size(); i++)
     {
         for(f = 0; f < 3; ++f)
         {
             for(e = 0; e < 3; ++e)
-                cout << M.problem[f][e] << "\t" ;
+                cout << vec.at(i).problem[f][e] << "\t" ;
             cout<<endl;
         }
         cout << endl;
